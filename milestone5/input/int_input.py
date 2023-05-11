670593be88502fdeb534cfcf936ad175b0cf63b9
JeremyCSUMB <jeshaw@csumb.edu>
2023-05-11 11:13:24 -0700

    Added milestone7 quiz input()

---
+++ milestone5/input/int_input.py
@@ -0,0 +1,14 @@
+##IGNORECASE false
+##IGNORESPACE false
+#Replace the . . . to complete the following code
+#It should print the hourly wage for the given 
+# number of hours (num_of_hours) which is taken as input
+# hourly rate (hourly_rate)
+# Output should be formatted as follows:
+# "Weekly wage = "calculated wage
+num_of_hours = int(input())
+hourly_rate = 15.0
+##HIDE
+weekly_wage = num_of_hours * hourly_rate
+print ("Weekly wage = %s" %weekly_wage)
+##EDIT . . .
---
+++ milestone5/input/str_input.py
@@ -0,0 +1,9 @@
+##IGNORECASE false
+##IGNORESPACE false
+#Replace the . . . with Python code
+#That will print
+#"What is your name? "
+#Take name as input and print "Hello " input name
+##HIDE
+print("Hello "+ input("What is your name? "))
+##EDIT . . .
